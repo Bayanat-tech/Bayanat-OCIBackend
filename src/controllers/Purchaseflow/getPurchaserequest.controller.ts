dd9040f9df8cd95ae9a5426aa2aa515a4e21bae9
UshaHipparkar <120270478+UshaHipparkar@users.noreply.github.com>
2025-12-13 11:00:23 +0530

    commit

--- src/controllers/Purchaseflow/getPurchaserequest.controller.ts
+++ src/controllers/Purchaseflow/getPurchaserequest.controller.ts
@@ -176,7 +176,8 @@ if (ls_request_number.includes("PO$")) {
       WHERE request_number = :REQ
       FETCH FIRST 1 ROWS ONLY
     `;
-
+console.log('prin',prinCode);
+console.log('prin',ls_request_number);
     const detailQuery = `
       SELECT *
       FROM VW_PURCHASE_REQUEST_TRANSACTION1
