55a43a1b201477bb5f41cd6b4668d571b13f5de6
Srishti nayal <Srishti.nayal@bayanattechnology.com>
2025-12-17 17:17:24 +0530

    delete supplier_master

--- src/routes/pf.routes.ts
+++ src/routes/pf.routes.ts
@@ -1,57 +1,3 @@
-// import * as express from "express";
-// import passport from "passport";
-
-
-// import {
-//   getPurchasefMaster,
-//   deletepfMaster,
-// } from "../../src/controllers/Purchaseflow/PurchaseFlowMaster.controller"
-
-
-// import gmPfRouter from "./Purchaseflow/gm_purchaseflow.routes";
-// import gmpurchaserequestRouter from "./Purchaseflow/transaction/gm_purchaserequest.routes";
-// import { checkUserAuthorization } from "../middleware/checkUserAthorization";
-
-// const router = express.Router();
-// // Route for transaction operations
-// router.use(
-//   "/:transaction",
-//   passport.authenticate("jwt", { session: false }),
-//   checkUserAuthorization,
-//   gmPfRouter
-// );
-// //gmpurchaserequestRouter
-// // Route for reports management
-// // router.use(
-// //   "/reports",
-// //   passport.authenticate("jwt", { session: false }),
-// //   checkUserAuthorization,
-// //
-// // );
-
-// router.get(
-//   "/:master",
-//   passport.authenticate("jwt", { session: false }),
-//   checkUserAuthorization,
-//   getPurchasefMaster
-// );
-
-// router.use(
-//   "/gm",
-//   passport.authenticate("jwt", { session: false }),
-//   checkUserAuthorization,
-//   gmPfRouter
-// );
-
-// router.post(
-//   "/:master",
-//   passport.authenticate("jwt", { session: false }),
-//   deletepfMaster
-// );
-// export default router;
-
-
-
 import * as express from "express";
 import passport from "passport";
 // import {
@@ -62,8 +8,7 @@ import passport from "passport";
 import gmPfRouter from "./Purchaseflow/gm_purchaseflow.routes";
 import gmpurchaserequestRouter from "./Purchaseflow/transaction/gm_purchaserequest.routes";
 import { checkUserAuthorization } from "../middleware/checkUserAthorization";
-//import { deletepfMaster, getPfMaster } from "../controllers/PurchaseFlow/pf.controller";
-import { getPurchasefMaster } from "../controllers/Purchaseflow/PurchaseFlowMaster.controller";
+import { getPurchasefMaster , deletepfMaster } from "../controllers/Purchaseflow/PurchaseFlowMaster.controller";
 
 const router = express.Router();
 // Route for transaction operations
@@ -97,10 +42,10 @@ router.use(
   gmPfRouter
 );
 
-// router.post(
-//   "/:master",
-//   passport.authenticate("jwt", { session: false }),
-//   deletepfMaster
-// );
+router.post(
+  "/:master",
+  passport.authenticate("jwt", { session: false }),
+  deletepfMaster
+);
 export default router;
 
