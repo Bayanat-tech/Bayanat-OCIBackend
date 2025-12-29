8bb7396aecca0d46efe5e6919fc1be7c45330771
sdandeka <sandeep.dandekar@bayanattechnology.com>
2025-12-29 16:50:28 +0530

    commit rahul

--- src/routes/Pams/pams.routes.ts
+++ src/routes/Pams/pams.routes.ts
@@ -6,7 +6,9 @@ import express, {
 } from "express";
 
 import {proc_build_dynamic_sql_PAMS} from "../../controllers/PAMS/proc_build_dynamic_sql_PAMS"
+import { proc_build_dynamic_del_PAMS } from "../../controllers/PAMS/proc_build_dynamic_del_PAMS";
 
 const router = express.Router();
 router.post("/proc_build_dynamic_sql_pams", proc_build_dynamic_sql_PAMS);
+router.post("/proc_build_dynamic_del_pams", proc_build_dynamic_del_PAMS);
 export default router;
