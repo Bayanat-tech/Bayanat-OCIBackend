2aac10a10887d62368b0e0e3048ec9c9b9bc570c
Sagar Buchkule <75515642+saisagar93@users.noreply.github.com>
2026-03-16 12:31:47 +0530

    commit

--- src/services/purchaseFlow/projectmaster.service.ts
+++ src/services/purchaseFlow/projectmaster.service.ts
@@ -1,65 +1,65 @@
-// import { getRepository } from "../../database/connection";
-// import { ProjectMaster } from "../../entity/PurchaseFlow/projectmaster.entity";
+import { getRepository } from "../../database/connection";
+import { ProjectMaster } from "../../entity/PurchaseFlow/projectmaster.entity";
 
 
-// export class ProjectMasterService {
-//   static getProjectMaster(loginid: string, page: number, limit: number): { fetchedData: any[]; totalCount: number; } | PromiseLike<{ fetchedData: any[]; totalCount: number; }> {
-//     throw new Error("Method not implemented.");
-//   }
-//   private static getRepository(company_code: string, page: number, limit: number) {
-//     return getRepository(ProjectMaster);
-//   }
+export class ProjectMasterService {
+  static getProjectMaster(loginid: string, page: number, limit: number): { fetchedData: any[]; totalCount: number; } | PromiseLike<{ fetchedData: any[]; totalCount: number; }> {
+    throw new Error("Method not implemented.");
+  }
+  private static getRepository() {
+    return getRepository(ProjectMaster);
+  }
 
-//   // Duplicate Check
-//   static async findDuplicate(
-//     project_code: string,
-//     company_code: string
-//   ): Promise<ProjectMaster | null> {
-//     const repo = this.getRepository();
+  // Duplicate Check
+  static async findDuplicate(
+    project_code: string,
+    company_code: string
+  ): Promise<ProjectMaster | null> {
+    const repo = this.getRepository();
 
-//     return await repo.findOne({
-//       where: { project_code, company_code }
-//     });
-//   }
+    return await repo.findOne({
+      where: { project_code, company_code }
+    });
+  }
 
-//   // Create
-//   // static async createProject(data: any): Promise<ProjectMaster> {
-//   //   const repo = this.getRepository();
+  // Create
+  static async createProject(data: any): Promise<ProjectMaster> {
+    const repo = this.getRepository();
 
-//   //   const project = repo.create({
-//   //     ...data,
-//   //     created_at: new Date(),
-//   //     updated_at: new Date()
-//   //   });
+    const project = repo.create({
+      ...data,
+      created_at: new Date(),
+      updated_at: new Date()
+    });
 
-//   //   return await repo.save(project);
-//   // }
+    return (await repo.save(project))[0];
+  }
 
-//   // Update
-//   static async updateProject(
-//     project_code: string,
-//     company_code: string,
-//     updateData: any
-//   ): Promise<boolean> {
-//     const repo = this.getRepository();
+  // Update
+  static async updateProject(
+    project_code: string,
+    company_code: string,
+    updateData: any
+  ): Promise<boolean> {
+    const repo = this.getRepository();
 
-//     const result = await repo.update(
-//       { project_code, company_code },
-//       {
-//         ...updateData,
-//         updated_at: new Date()
-//       }
-//     );
+    const result = await repo.update(
+      { project_code, company_code },
+      {
+        ...updateData,
+        updated_at: new Date()
+      }
+    );
 
-//     return result.affected ? result.affected > 0 : false;
-//   }
+    return result.affected ? result.affected > 0 : false;
+  }
 
-//   // Delete
-//   static async deleteProjects(projectCodes: string[]): Promise<number> {
-//     const repo = this.getRepository();
+  // Delete
+  static async deleteProjects(projectCodes: string[]): Promise<number> {
+    const repo = this.getRepository();
 
-//     const result = await repo.delete(projectCodes);
+    const result = await repo.delete(projectCodes);
 
-//     return result.affected ? result.affected : 0;
-//   }
-// }
+    return result.affected ? result.affected : 0;
+  }
+}
