d377b90adfab20f4912ad14e48b31cbf5e57fb97
Sagar Buchkule <75515642+saisagar93@users.noreply.github.com>
2026-07-20 16:04:52 +0530

    Add Fright

--- src/routes/Freight/freight.routes.ts
+++ src/routes/Freight/freight.routes.ts
@@ -4,6 +4,11 @@ import { Router } from "express";
 
 import { tenantContextMiddleware } from "../../../src/middleware/tenantContext.middleware";
 import { tenantMiddleware } from "../../../src/middleware/tenant.middleware";
+import {
+  proc_build_dynamic_del_FREIGHT,
+  proc_build_dynamic_ins_upd_FREIGHT,
+  proc_build_dynamic_sql_FREIGHT,
+} from "../../controllers/Freight/freightDynamicProcedures";
 import { insUpdTfEnquiryBulk } from "../../controllers/Freight/insUpdTfEnquiryBulk";
 
 const router = express.Router();
@@ -14,6 +19,21 @@ router.use(tenantContextMiddleware);
 router.post(
   "/insUpdTfEnquiryBulk",
   insUpdTfEnquiryBulk 
+);
+
+router.post(
+  "/gm/proc_build_dynamic_sql_freight",
+  proc_build_dynamic_sql_FREIGHT
+);
+
+router.post(
+  "/gm/proc_build_dynamic_ins_upd_freight",
+  proc_build_dynamic_ins_upd_FREIGHT
+);
+
+router.post(
+  "/gm/proc_build_dynamic_del_freight",
+  proc_build_dynamic_del_FREIGHT
 );
 
 
