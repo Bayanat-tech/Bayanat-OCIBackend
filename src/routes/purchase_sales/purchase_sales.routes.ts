d87f03e65860472c8de1bfcac6df89213134dafc
Rashmita Mhatre <285643138+Rashmita-bayanat@users.noreply.github.com>
2026-07-26 12:45:39 +0530

    Feat: purchase&sales

---
+++ src/routes/purchase_sales/purchase_sales.routes.ts
@@ -0,0 +1,23 @@
+import * as express from "express";
+import passport from "passport";
+import { Router } from "express";
+
+import { tenantContextMiddleware } from "../../../src/middleware/tenantContext.middleware";
+import { tenantMiddleware } from "../../../src/middleware/tenant.middleware";
+import { insUpdTtePOrderBulk } from "../../controllers/purchase_sales/insUpdTtePOrderBulk";
+
+const router = express.Router();
+router.use(tenantMiddleware);
+router.use(tenantContextMiddleware);
+
+
+router.post(
+  "/insUpdTtePOrderBulk",
+  insUpdTtePOrderBulk 
+);
+
+
+ export default router;
+
+
+
