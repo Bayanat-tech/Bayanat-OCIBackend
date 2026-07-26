41f4b80e63552c7a0742e3f19487da2f7f6763f7
Rashmita Mhatre <285643138+Rashmita-bayanat@users.noreply.github.com>
2026-07-26 13:36:28 +0530

    fix: route of ps

--- src/routes/purchase_sales/purchase_sales.routes.ts
+++ src/routes/purchase_sales/purchase_sales.routes.ts
@@ -5,16 +5,23 @@ import { Router } from "express";
 import { tenantContextMiddleware } from "../../../src/middleware/tenantContext.middleware";
 import { tenantMiddleware } from "../../../src/middleware/tenant.middleware";
 import { insUpdTtePOrderBulk } from "../../controllers/purchase_sales/insUpdTtePOrderBulk";
+import { insUpdTtePQuotationBulk } from "../../controllers/purchase_sales/insUpdTtePQuotationBulk";
 
 const router = express.Router();
 router.use(tenantMiddleware);
 router.use(tenantContextMiddleware);
+
+router.post(
+  "/insUpdTtePQuotationBulk",
+  insUpdTtePQuotationBulk 
+);
 
 
 router.post(
   "/insUpdTtePOrderBulk",
   insUpdTtePOrderBulk 
 );
+
 
 
  export default router;
