e01e1cdd840ea824830db48ce7f5e50ee9c9ac3d
Sagar Buchkule <75515642+saisagar93@users.noreply.github.com>
2026-08-03 13:34:53 +0530

    Update common.interface.ts

--- src/interfaces/common.interface.ts
+++ src/interfaces/common.interface.ts
@@ -39,7 +39,6 @@ export interface TreeNode {
   level3?: string | null;
   position?: number | null;
   children?: TreeNode[];
-  position?: number;
 }
 export type TLogin = { email: string; password: string };
 export interface ComparePasswordInterface {
