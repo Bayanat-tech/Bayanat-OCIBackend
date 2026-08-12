5e74af73858eab518674cafbb0a225895f9f8530
Rashmita Mhatre <285643138+Rashmita-bayanat@users.noreply.github.com>
2026-08-12 17:13:51 +0530

    fix: routes

--- src/routes/purchase_sales/purchase_sales.routes.ts
+++ src/routes/purchase_sales/purchase_sales.routes.ts
@@ -12,9 +12,15 @@ import { insUpdTteSOrderBulk } from "../../controllers/purchase_sales/insUpdTteS
 import { insUpdTteSdnBulk } from "../../controllers/purchase_sales/insUpdTteSdnBulk";
 import { insUpdTteTransferBulk } from "../../controllers/purchase_sales/insUpdTteTransferBulk";
 import { insUpdTteAdjustmentBulk } from "../../controllers/purchase_sales/insUpdTteAdjustmentBulk";
+<<<<<<< Updated upstream
 import { insUpdJobProduction } from "../../controllers/purchase_sales/insUpdJobProduction";
 // TODO: fix this import path to wherever insUpdMfBom actually lives
 import { insUpdMfBom } from "../../controllers/purchase_sales/insUpdMfBom";
+=======
+import {insUpdJobProduction} from "../../controllers/purchase_sales/insUpdJobProduction"
+import { insUpdTtePInvoiceBulk } from "../../controllers/purchase_sales/insUpdTtePInvoiceBulk";
+import { insUpdTteSinvoice } from "../../controllers/purchase_sales/insUpdTteSinvoice";
+>>>>>>> Stashed changes
 
 const router = express.Router();
 router.use(tenantMiddleware);
@@ -65,6 +71,18 @@ router.post(
   "/insUpdJobProduction",
   insUpdJobProduction    
 );
+
+router.post(
+  "/insUpdTtePInvoiceBulk",
+  insUpdTtePInvoiceBulk    
+);
+
+router.post(
+  "/insUpdTteSinvoice",
+  insUpdTteSinvoice    
+);
+export default router;
+
 
 router.post(
   "/insUpdMfBom",
